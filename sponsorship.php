8803fb833de8d1806b5bcfc1dd6e24dedb22f990
Daniel Li <lidaniel.sb@gmail.com>
2021-06-02 19:39:07 +1000

    changed amp logo

--- sponsorship.php
+++ sponsorship.php
@@ -17,7 +17,7 @@ get_template_part('index','banner'); ?>
     <main id="content">
     <div class="container">
         <!-- this one above is the container class from the new thing everything inbetween -->
-        <div class="pageSponsors"> 
+        <div class="pageSponsors">
             <a name="sponsors"></a>
             <div class="section">
                 <div class="sponsors-container container">
@@ -30,8 +30,8 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.hsbc.com.au/",
                                 "div_id"    => "HSBC",
-                                "name"      => "HSBC", 
-                                "image_file"=> "2021/01/hsbc3.png", 
+                                "name"      => "HSBC",
+                                "image_file"=> "2021/01/hsbc3.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -40,7 +40,7 @@ get_template_part('index','banner'); ?>
                     <div class="subheading">
                         <br/>
                         <h1 class="h2Custom">GOLD SPONSORS</h1>
-                        
+
                         <hr/>
                     </div>
                     <div class="sponsorsOfficial">
@@ -48,18 +48,18 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.reckitt.com/",
                                 "div_id"    => "reckitt",
-                                "name"      => "reckitt", 
-                                "image_file"=> "2021/05/Reckitt_2021.svg_.png", 
+                                "name"      => "reckitt",
+                                "image_file"=> "2021/05/Reckitt_2021.svg_.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
-                        ?>    
+                        ?>
                         <?php
                             $args = array(
                                 "site_url"  => "https://www.franklintempleton.com.au/",
                                 "div_id"    => "bcr",
-                                "name"      => "Franklin Templeton Investments", 
-                                "image_file"=> "2021/01/Franklin.jpg", 
+                                "name"      => "Franklin Templeton Investments",
+                                "image_file"=> "2021/01/Franklin.jpg",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -69,19 +69,19 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.tmgm.com.au/en",
                                 "div_id"    => "TMGM",
-                                "name"      => "TMGM", 
-                                "image_file"=> "2021/05/tmgm.png", 
+                                "name"      => "TMGM",
+                                "image_file"=> "2021/05/tmgm.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
-                        ?>   
+                        ?>
                         <br/>
-                        
+
                     </div>
                     <div class="subheading">
                         <br/>
                         <h1 class="h2Custom">SILVER SPONSORS</h1>
-                        
+
                         <hr/>
                     </div>
 
@@ -90,8 +90,8 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.bain.com/",
                                 "div_id"    => "bain",
-                                "name"      => "bain", 
-                                "image_file"=> "2021/01/bain.png", 
+                                "name"      => "bain",
+                                "image_file"=> "2021/01/bain.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -100,8 +100,8 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.neu.capital/",
                                 "div_id"    => "neucapital",
-                                "name"      => "Neucapital", 
-                                "image_file"=> "2021/05/neucapital2-scaled.jpg", 
+                                "name"      => "Neucapital",
+                                "image_file"=> "2021/05/neucapital2-scaled.jpg",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -110,14 +110,14 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.greenstonepartners.com.au/",
                                 "div_id"    => "Greenstone",
-                                "name"      => "Greenstone", 
-                                "image_file"=> "2021/05/greenstonepartners.png", 
+                                "name"      => "Greenstone",
+                                "image_file"=> "2021/05/greenstonepartners.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
                         ?>
                     </div>
-            
+
                     <div class="subheading">
                         <br/>
                         <h1 class="h2Custom">BRONZE SPONSORS</h1>
@@ -128,8 +128,8 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.ampcapital.com/au/en/home",
                                 "div_id"    => "AMP",
-                                "name"      => "AMP", 
-                                "image_file"=> "2021/01/amp.jpg", 
+                                "name"      => "AMP",
+                                "image_file"=> "2021/06/Amp-capital-logo.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -148,8 +148,8 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://longreachalternatives.com/",
                                 "div_id"    => "longreach",
-                                "name"      => "longreach", 
-                                "image_file"=> "2021/05/longreach.jpg", 
+                                "name"      => "longreach",
+                                "image_file"=> "2021/05/longreach.jpg",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -158,26 +158,26 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.bloomberg.com/asia",
                                 "div_id"    => "Bloomberg",
-                                "name"      => "Bloomberg", 
-                                "image_file"=> "2021/05/Bloomberg.jpg", 
+                                "name"      => "Bloomberg",
+                                "image_file"=> "2021/05/Bloomberg.jpg",
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
-                        ?> 
+                        ?>
                         <?php
                             $args = array(
                                 "site_url"  => "https://www.pottinger.com/",
                                 "div_id"    => "Pottinger",
-                                "name"      => "Pottinger", 
-                                "image_file"=> "2021/06/pottinger.jpg", 
+                                "name"      => "Pottinger",
+                                "image_file"=> "2021/06/pottinger.jpg",
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
                         ?>
-                    </div>       
+                    </div>
                     <br/>
                     <div class="subheading">
                         <br/>
                         <h1 class="h2Custom"><span style="color:#553397">MARKETING</span> SPONSORS</h1>
-                        
+
                         <hr/>
                     </div>
                     <div class="sponsorsOfficial">
@@ -185,8 +185,8 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://www.flowtraders.com/",
                                 "div_id"    => "flowtraders",
-                                "name"      => "flowtraders", 
-                                "image_file"=> "2021/01/ft.png", 
+                                "name"      => "flowtraders",
+                                "image_file"=> "2021/01/ft.png",
                                 "detail"    => ""
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
@@ -195,27 +195,27 @@ get_template_part('index','banner'); ?>
                             $args = array(
                                 "site_url"  => "https://cfas.org.au/about/cfa-society-sydney/",
                                 "div_id"    => "CFA",
-                                "name"      => "CFA", 
-                                "image_file"=> "2021/06/CFA-e1622626128904.jpg", 
+                                "name"      => "CFA",
+                                "image_file"=> "2021/06/CFA-e1622626128904.jpg",
                             );
                             get_template_part("templates/individual_sponsor", null, $args);
-                        ?> 
-                    </div>  
+                        ?>
+                    </div>
                 </div>
                 <div class="section2 applyContainer">
                     <a name="apply"></a>
                     <div class="title">
                         <h1>APPLY</h1>
                     </div>
-                
+
                     <a href="mailto:sponsorship@unswfinsoc.org.au">
-                        <div class="apply">   
+                        <div class="apply">
                             <button type="button" class="btn btn-outline-primary applyButton">Help sponsor FINSOC by sending us an email at <br/><b>sponsorship@unswfinsoc.org.au</b></button>
                         </div>
                     </a>
                     <br/>
                     <br/>
-                    <br/>    
+                    <br/>
                 </div>
             </div>
         </div>
